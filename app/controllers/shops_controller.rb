1ff152ab8495bf2ae311a513e5452f01d89bf4e8
james <j.w.lindsey1986@gmail.com>
2017-06-10 13:55:00 -0500

    fixed shop submission form

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -7,4 +7,15 @@ class ShopsController < ApplicationController
   def new
     @shop = Shop.new
   end
+
+  def create
+    Shop.create(shop_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :description, :address)
+  end
 end
