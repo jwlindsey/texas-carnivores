98901fb22786b40bd83d6ebb11072b1e68b77398
james <j.w.lindsey1986@gmail.com>
2017-06-16 20:51:33 -0500

    added the comment form submission process

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @shop = Shop.find(params[:shop_id])
+    @shop = comments.create(comment_params.merge(user: current_user))
+    redirect_to shop_path(@shop)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message, :rating)
+  end
+end
