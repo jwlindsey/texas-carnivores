45092aee41c19585806ffbfa40efb06ddfc55c61
james <j.w.lindsey1986@gmail.com>
2017-06-18 16:46:16 -0500

    corrected routes for initial photo hook up

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   root 'shops#index'
   resources :shops do
     resources :comments, only: :create
+    resources :photos, only: :create
+  end
+  resources :users, only: :show
+  resources :index do
+    resources :photos, only: :show
   end
 end
